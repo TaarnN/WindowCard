04c201bdf0a68673066f091d911f64ac6ab9698b
TaarnN <taarn.ng@gmail.com>
2025-02-23 10:18:20 +0700

    push files

---
+++ Documents/PGM/VSKORNLang/With_Korn/window-card-game/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{ textAlign: "center", fontSize: "x-large", fontWeight: "bold" }}
+    >
+      Hello, project is set up already
+    </div>
+  );
+};
+
+export default App;
